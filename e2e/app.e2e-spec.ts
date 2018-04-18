fead5fb25f2d6c7505b5a316134db48ae0e2fa7c
Daniel Dimitrov <dakata962@gmail.com>
2018-04-18 13:40:41 +0300

    NG bug fixed

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AppPage } from './app.po';
+import { ProjectPage } from './app.po';
 
-describe('project-yarn App', () => {
-  let page: AppPage;
+describe('project App', function() {
+  let page: ProjectPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new ProjectPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('app works!');
   });
 });
