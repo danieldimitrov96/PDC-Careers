c9e8dc0116a3b0dfceee79071b96259f0c5f1eb7
Daniel Dimitrov <dakata962@gmail.com>
2018-04-22 16:03:47 +0300

    Add template for home

--- src/app/careers/careers.component.ts
+++ src/app/careers/careers.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-careers',
   templateUrl: './careers.component.html',
-  styleUrls: ['./careers.component.css']
+  styleUrls: ['./careers.component.css'],
 })
-export class CareersComponent implements OnInit {
 
-  constructor() { }
+export class CareersComponent implements OnInit {
+  public jobTypes = ['All', 'IT', 'HR'];
+  public jobSelected = 'All';
+
+  constructor() {
+   }
 
   ngOnInit() {
+
   }
 
 }
