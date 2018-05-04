8e9079632d2c3daa5182f85a5d39c8fcd138237f
Daniel Dimitrov <dakata962@gmail.com>
2018-05-04 19:13:40 +0300

    Done remember me

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,10 @@
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 
 import { AuthService } from '../../core/auth.service';
+import { ILocalStorage } from '../../models/ILocalStorage/ILocalStorage';
 import { User } from '../../models/user/user';
 
 @Component({
@@ -11,23 +12,50 @@ import { User } from '../../models/user/user';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   public loginForm: User;
-  private unautorizedStatus: number = 401;
+  public localS: ILocalStorage = {
+    email: '',
+    password: '',
+    remember: 'false',
+  };
+  public rememberMe = false;
 
-  constructor(private authService: AuthService, private toastr: ToastrService) { }
+  private unautorizedStatus: number = 401;
+  constructor(private authService: AuthService, private toastr: ToastrService) {}
 
   public login(form: NgForm, route: string): void {
     this.loginForm = form.value;
+    this.localS = form.value;
+    if (!this.localS.remember) {
+      this.localS.remember = 'false';
+    }
     this.authService.loginOrSignup(this.loginForm, route).subscribe(
       (res) => {
         this.toastr.success(`Welcome, ${this.loginForm.email}`);
+        if (this.localS.remember) {
+          localStorage.setItem('email', this.localS.email);
+          localStorage.setItem('password', this.localS.password);
+          localStorage.setItem('rememberMe', this.localS.remember);
+          this.rememberMe = true;
+        } else {
+          this.rememberMe = false;
+          localStorage.setItem('rememberMe', this.localS.remember);
+        }
       },
       (err: HttpErrorResponse) => {
         if (err.status === this.unautorizedStatus) {
           this.toastr.error('Invalid email or password!');
         }
       });
+  }
+  public ngOnInit(): void {
+    if (localStorage.rememberMe === 'true') {
+      this.localS.email = localStorage.email;
+      this.localS.password = localStorage.password;
+      this.localS.remember = localStorage.remember;
+      this.rememberMe = true;
+    }
   }
 }
---
+++ src/app/models/ILocalStorage/ILocalStorage.ts
@@ -0,0 +1,5 @@
+export interface ILocalStorage {
+    email: string;
+    password: string;
+    remember: string;
+}
